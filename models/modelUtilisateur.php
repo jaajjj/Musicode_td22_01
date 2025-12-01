c639a6eaa20a75c182b14374ee1b4863eabba2a0
matthieu <matthieu.marhdour@gmail.com>
2025-12-01 14:22:31 +0100

    fontction inscription ajouter

--- models/modelUtilisateur.php
+++ models/modelUtilisateur.php
@@ -7,4 +7,13 @@ function get_user(){
     $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $users;
 }
+
+function inscription($username, $password, $email){
+    $pdo = get_bdd();
+    $stmt = $pdo->prepare("
+        INSERT INTO user (nom_user,mail_user,pwd_user )
+        VALUES (?, ?, ?)
+    ");
+    return $stmt->execute([$username, $email, $password]);
+}
 var_dump(get_user());
