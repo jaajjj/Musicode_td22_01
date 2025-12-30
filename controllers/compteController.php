b5c78f461cc49abb15efd73a3006c27905ae1503
Sami Hajadi <samihajadi@gmail.com>
2025-12-30 17:20:32 +0100

    Correction page moncompte

--- controllers/compteController.php
+++ controllers/compteController.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../models/modelCompte.php';
+require_once __DIR__ . '/../models/modelUtilisateur.php';
 
 $idUser = (int) $_SESSION['user']['id_user'];
 $user   = get_compte_user($idUser);
--- models/modelUtilisateur.php
+++ models/modelUtilisateur.php
@@ -35,3 +35,27 @@ function verify_user(string $email, string $password) {
     }
     return false;
 }
+
+function get_compte_user(int $idUser) {
+    $pdo = get_bdd();
+    $sql = "SELECT id_user, nom_user, mail_user, mdp_user
+            FROM USER
+            WHERE id_user = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$idUser]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function update_compte_nom(int $idUser, string $nom): bool {
+    $pdo = get_bdd();
+    $sql = "UPDATE USER SET nom_user = ? WHERE id_user = ?";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute([$nom, $idUser]);
+}
+
+function update_compte_nom_mdp(int $idUser, string $nom, string $hashedPassword): bool {
+    $pdo = get_bdd();
+    $sql = "UPDATE USER SET nom_user = ?, mdp_user = ? WHERE id_user = ?";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute([$nom, $hashedPassword, $idUser]);
+}
